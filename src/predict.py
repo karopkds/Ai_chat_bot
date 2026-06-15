054122efbae5d71f341a6a2fcb5d0e83a8313a2b
karopkds <karopdezosa541999@gmail.com>
2026-06-15 04:08:08 -0400

    joing the prepossing function in predict.py file for the correct flow

--- src/predict.py
+++ src/predict.py
@@ -1,5 +1,9 @@
 import joblib
 from response_engine import get_response
+from preprocessing import preprocess
+from stop_words_removal import remove_stopwords
+from lemmatizer import lemmatize
+
 
 model = joblib.load("models/model.pkl")
 
@@ -14,13 +18,23 @@ while True:
     if user_input.lower() == "exit":
         print("GOODBYE! See you Soon :)")
         break
-    user_vector = vectorizer.transform([user_input])
 
-    prediction = model.predict(user_vector)
+    #NLP PreProcessing
+    tokenization_words_final = preprocess(user_input)
+
+    rm_stopped_words_final = remove_stopwords(tokenization_words_final)
+
+    lemmit_words_final = lemmatize(rm_stopped_words_final)
+
+    processed_text = " ".join(lemmit_words_final)
+
+    vectorization_final = vectorizer.transform([processed_text])
+
+    prediction = model.predict(vectorization_final)
 
     intent = label_encoder.inverse_transform(prediction)
 
 
     response = get_response(intent[0])
-    
+   
     print("KDS_BOT: ", response)
