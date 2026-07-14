b3967906d9f2cd0c4dfd1c169e3d9c6019a91bf7
karopkds <karopdezosa541999@gmail.com>
2026-07-14 19:51:51 -0400

    added precison,f1score,recall and support

--- src/train.py
+++ src/train.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import LabelEncoder
 from sklearn.naive_bayes import MultinomialNB
@@ -7,10 +8,16 @@ from preprocessing import preprocess
 from stop_words_removal import remove_stopwords
 from lemmatizer import lemmatize
 import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+
 
 df = pd.read_csv("data/intents.csv")
 # Assigning X and Y access
 processed_sentences = []
+
 
 for sentence in df["sentence"]:
 
@@ -29,22 +36,75 @@ X = processed_sentences
 
 Y = df["intent"]
 
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X,
+    Y,
+    test_size=0.2,
+    random_state=42,
+    stratify= Y
+)
+
 # Now i'm converting into vector formate so ML can understand
 vectorizing_Traning = TfidfVectorizer()
-X_tfidf = vectorizing_Traning.fit_transform(X)
+X_train_tfidf = vectorizing_Traning.fit_transform(X_train)
+X_test_tfidf = vectorizing_Traning.transform(X_test)
+
+
 #print(X_tfidf)
 
 # Convert labels into numbers
 label_encoder = LabelEncoder()
-Y_encoded = label_encoder.fit_transform(Y)
-print(Y_encoded)
+Y_train_encoded = label_encoder.fit_transform(Y_train)
+Y_test_encoded = label_encoder.transform(Y_test)
+print(Y_train_encoded)
 
 # Now the Actual Trainig Begins
 # We are using Navie Byes MultinimialNB (Best for Spam Detection, Sentimental Analysis, Email Categorization)
 
 model = MultinomialNB()
 model = CalibratedClassifierCV(model, cv=3, method='isotonic')
-model.fit(X_tfidf, Y_encoded)
+model.fit(X_train_tfidf, Y_train_encoded)
+Y_pred = model.predict(X_test_tfidf)
+accuracy = accuracy_score(Y_test_encoded, Y_pred)
+print(f"Model Accuracy: {accuracy:.2%}")
+print("\nClassification Report:\n")
+
+print(
+    classification_report(
+        Y_test_encoded,
+        Y_pred,
+        target_names=label_encoder.classes_,
+        zero_division=0
+    )
+)
+cm = confusion_matrix(Y_test_encoded, Y_pred)
+
+plt.figure(figsize=(8, 6))
+
+plt.imshow(cm)
+
+plt.title("Confusion Matrix")
+
+plt.colorbar()
+
+plt.xticks(
+    range(len(label_encoder.classes_)),
+    label_encoder.classes_,
+    rotation=45,
+)
+
+plt.yticks(
+    range(len(label_encoder.classes_)),
+    label_encoder.classes_,
+)
+
+plt.xlabel("Predicted Label")
+
+plt.ylabel("True Label")
+
+plt.tight_layout()
+
+plt.show()
 
 # NOW WE ARE SAVING THE MODEL Traing data. So we are using joblib Libarrary
 # So we dont want to train again and again when we retstart the VSCODE
